261cc9841beff543257c27053565b9460feadce9
allen <allen@metaderby.com>
2022-11-30 14:15:07 +0800

    img

--- src/components/login/Welcome/Welcome.js
+++ src/components/login/Welcome/Welcome.js
@@ -6,7 +6,7 @@ import classes from './Welcome.module.css'
 const Welcome = () => {
   return (
     <div className={classes.Content}>
-      <img src={Icon}/>
+      <img src={Icon} alt=""/>
       <h1>欢迎回来！</h1>
       <p>即将进入去中心化网络</p>
     </div>
--- src/components/login/header/Header.js
+++ src/components/login/header/Header.js
@@ -5,7 +5,7 @@ import Logo from '../../../asset/icon.png'
 const Header = () => {
   return (
     <div className={classes.Header}>
-      <img src={Logo} className={classes.Icon}/>
+      <img src={Logo} className={classes.Icon} alt=""/>
       <select className={classes.Select}>
         <option>
           Avalanche C-Chain
